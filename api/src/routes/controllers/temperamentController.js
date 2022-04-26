3b421f4785d710d2c4c08b92991c31c7f4798a96
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-25 21:42:10 -0300

    temperaments loaded to Db

--- api/src/routes/controllers/temperamentController.js
+++ api/src/routes/controllers/temperamentController.js
@@ -12,8 +12,35 @@ const {
 } = process.env;
 
 
-async function getAllTemperaments(req, res, next) {
-
+async function getAllTemperamentsFromApi(req, res, next) {
+  try {
+    let breedsFromApi = await axios({
+        headers: {
+          'x-api-key': API_KEY
+        },
+        method: 'get',
+        url: "https://api.thedogapi.com/v1/breeds"
+      })
+      .then(results => results.data)
+      .then(results => results.map(breed => breed.temperament))
+      .then(results => results.filter(temperaments => temperaments != null))
+      .then(results => results.map(temperaments => temperaments.split(', ')))
+      .then(results => results.flat())
+      .then(results => results.filter((item, index) => {
+        return results.indexOf(item) === index;
+      }))
+      .then(results => results.map(temperament => {
+        return {
+          name: temperament
+        }
+      }))
+      .then(results => Temperament.bulkCreate(results))
+      .then(results => res.send('anduvo todo bien'))
+  } catch (error) {
+    next(error)
+  }
 }
 
-module.exports = { getAllTemperaments}
+module.exports = {
+  getAllTemperamentsFromApi
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { getAllDogs, getDogById, createDog } = require('./controllers/breedController')
-const { getAllTemperaments} = require('./controllers/temperamentController')
+const { getAllTemperamentsFromApi} = require('./controllers/temperamentController')
 
 
 const router = Router();
@@ -9,7 +9,7 @@ const router = Router();
 
 router.get('/dogs', getAllDogs);
 router.get('/dogs/:id', getDogById);
-router.get('/temperament', getAllTemperaments);
+router.get('/temperament', getAllTemperamentsFromApi);
 router.post('/dogs', createDog);
 router.get('*', function (req, res) {
   res.status(404).send('Ruta no encontrada');
