2ed4a0e8845d0daaa509b3b6f4810a5c5b89fc4b
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-05-01 08:34:50 -0300

    basic of styled-components

---
+++ client/src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from './common';
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script';
+  }
+`
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>
+        Primary
+      </Button>
+      < Button secondary>
+        Secondary
+      </Button>
+      <Button large>
+        Large
+      </Button>
+      
+    </>
+
+  );
+}
+
+export default App;
