ba31af04b757b11861ef0b237dcdb665496583d2
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-05-12 16:08:09 -0300

    datos de dog creados llegan a pag detail, pero falta corregir temperaments detail

--- api/src/routes/controllers/breedController.js
+++ api/src/routes/controllers/breedController.js
@@ -21,9 +21,28 @@ async function getAllDogs(req, res, next) {
   }
 
   try {
-    // aqui tendria que hacer algo similar a lo que hago abajo con API creando un campo temperament
-    // en el que transformo el arreglo de temperamentos en un string o en un array?
-    breedsFromDb = await Breed.findAll();
+    breedsFromDb = await Breed.findAll({
+      include: [
+        {
+          model: Temperament,
+          // as: "temperament",
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    let filteredBreedsDb = breedsFromDb.map((breed) => {
+      return {
+        id: breed.id,
+        name: breed.name,
+        height: breed.height.metric,
+        weight: breed.weight.metric,
+        temperaments: breed.temperament ? breed.temperament.split(", ") : [],
+        life_span: breed.weight.metric ? breed.weight.metric : "",
+        image: breed.image.url ? breed.image.url : "",
+      };
+    });
+
   } catch (error) {
     next(error);
   }
@@ -34,11 +53,11 @@ async function getAllDogs(req, res, next) {
       // porque usaba breedsApi.map????
       let filteredBreedsApi = breedsFromApi.map((breed) => {
         return {
-          id: breed.id,
+          id: breed.id.toString(),
           name: breed.name,
           height: breed.height.metric,
           weight: breed.weight.metric,
-          temperament: breed.temperament ? breed.temperament.split(", ") : [],
+          temperaments: breed.temperament ? breed.temperament.split(", ") : [],
           life_span: breed.weight.metric ? breed.weight.metric : "",
           image: breed.image.url ? breed.image.url : "",
         };
@@ -47,61 +66,6 @@ async function getAllDogs(req, res, next) {
       // console.log(BreedsDB);
       let allBreeds = [...filteredBreedsApi, ...breedsFromDb];
       res.status(200).send(allBreeds);
-      
-    })
-    .catch((error) => next(error));
-}
-
-async function getDogById(req, res, next) {
-  const { id } = req.params;
-
-  let breedsFromApi;
-  let breedsFromDb;
-
-  try {
-    breedsFromApi = await axios({
-      headers: {
-        "x-api-key": API_KEY,
-      },
-      method: "get",
-      url: "https://api.thedogapi.com/v1/breeds",
-    }).then((results) => results.data);
-  } catch (error) {
-    next(error);
-  }
-
-  try {
-    // aqui tendria que hacer algo similar a lo que hago abajo con API creando un campo temperament
-    // en el que transformo el arreglo de temperamentos en un string o en un array?
-    breedsFromDb = await Breed.findAll();
-  } catch (error) {
-    next(error);
-  }
-
-  Promise.all([breedsFromApi, breedsFromDb])
-    .then((respuesta) => {
-      let filteredBreedsApi = breedsFromApi.map((breed) => {
-        return {
-          id: breed.id,
-          name: breed.name,
-          height: breed.height.metric,
-          weight: breed.weight.metric,
-          temperament: breed.temperament ? breed.temperament.split(", ") : [],
-          life_span: breed.weight.metric ? breed.weight.metric : "",
-          image: breed.image.url ? breed.image.url : "",
-        };
-      });
-
-      let allBreeds = [...filteredBreedsApi, ...breedsFromDb];
-
-      if (id) {
-          let breed = allBreeds.filter((el) => el.id == id);
-          breed.length
-            ? res.status(200).json(breed)
-            : res
-                .status(404)
-                .send(`Sorry, we don´t have a breed with ${id} as ID 🤷‍♀️`);
-        }
     })
     .catch((error) => next(error));
 }
@@ -144,6 +108,6 @@ async function createDog(req, res, next) {
 
 module.exports = {
   getAllDogs,
-  getDogById,
+  // getDogById,
   createDog,
 };
